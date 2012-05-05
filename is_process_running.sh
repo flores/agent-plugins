373fd6bf0e880c6aa313563e07302dc6cabb6249
cflores <flores@petalphile.com>
2012-05-04 17:09:34 -0700

    add ability to expect a number of processes.

--- is_process_running.sh
+++ is_process_running.sh
@@ -20,17 +20,31 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <process>"
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <process> <expected number of processes>"
+  echo "ex: $0 apache 5"
+  echo -e "<expected number of processes> is optional and defaults to 1\n"
   exit
 fi
 
 PROCESS_NAME=$1
 
+if [ -z $2 ]; then
+  EXPECTED_PROCESSES=1
+else
+  EXPECTED_PROCESSES=$2
+fi
+
 NUM_PROCESSES=`ps ax | grep "$PROCESS_NAME" | grep -v grep | wc -l`
 
-if [ "$NUM_PROCESSES" -lt "1" ]; then
-  echo "status err $PROCESS_NAME is not running"
-else
+if [ -z $NUM_PROCESSES ]; then
+  NUM_PROCESSES=0
+fi
+
+if [ "$NUM_PROCESSES" -eq "$EXPECTED_PROCESSES" ]; then
   echo "status ok ok"
+elif [ "$NUM_PROCESSES" -lt "$EXPECTED_PROCESSES" ]; then
+  echo "status err $PROCESS_NAME has less than $EXPECTED_PROCESSES processes running"
+else
+  echo "status warn $PROCESS_NAME has $NUM_PROCESSES processes running.  expected $EXPECTED_PROCESSES processes running"
 fi
