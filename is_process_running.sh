cef2b67ba8c83e52cf3e56869747dcfcad073cc3
cflores <flores@petalphile.com>
2012-05-04 17:11:55 -0700

    @cloudkick requires a license

--- is_process_running.sh
+++ is_process_running.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # License: MIT
+# Copyright: Carlo Flores <github@petalphile.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
